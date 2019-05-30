36e573f69e6c67b9bee50dd4e78ddb85c9e89812
patti-c <patticarter@digitree.com>
2019-05-30 14:45:36 -0400

    adds hand and card classes

--- blackjack.py
+++ blackjack.py
@@ -15,9 +15,49 @@ class Card:
     def __init__(self, suite, number):
         self.suite = suite
         self.number = number
+        self.determine_value()
 
     def __str__(self):
-        return f"The {self.number} of {self.suite}"
+        if self.isNumber():
+            return f"The {self.number} of {self.suite}"
+        elif self.number == 11:
+            return f"The Jack of {self.suite}"
+        elif self.number == 12:
+            return f"The Queen of {self.suite}"
+        elif self.number == 13:
+            return f"The King of {self.suite}"
+        elif self.isAce():
+            return f"The Ace of {self.suite}"
+
+    def determine_value(self):
+        if(self.isNumber()):
+            self.value = self.number
+        elif(self.isFace()):
+            self.value = 10
+        elif(self.isAce()):
+            self.value = 1
+
+    def isNumber(self):
+        return self.number <= 10
+
+    def isFace(self):
+        return self.number > 10 and self.number < 14
+
+    def isAce(self):
+        return self.number == 14
+
+    def draw(self):
+        return self.cards.pop()
+
+class Hand:
+    def __init__(self, card):
+        cards = []
+        cards.append(card)
+        self.cards = []
+
+class Player:
+    def __init__(self, hand):
+        self.hand = hand
 
 mydeck = Deck()
-for card in mydeck.cards: print(card)
+for card in mydeck.cards: print(card.value)
